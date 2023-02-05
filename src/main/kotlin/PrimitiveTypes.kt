0b04faca986ea0babd3d7de399d7e13e9616fe3f
Sonal Dhage <sonal.dhage@thermondo.de>
2023-02-05 16:28:05 +0100

    Created an example for primitive types

---
+++ src/main/kotlin/PrimitiveTypes.kt
@@ -0,0 +1,29 @@
+fun main() {
+    // Int type
+    var age: Int = 24
+    println("Age: $age")
+
+    // Boolean type
+    var isAdult: Boolean = age >= 18
+    println("Is adult: $isAdult")
+
+    // Byte type
+    var byteValue: Byte = 120
+    println("Byte value: $byteValue")
+
+    // Short type
+    var shortValue: Short = 20000
+    println("Short value: $shortValue")
+
+    // Long type
+    var longValue: Long = 750000000
+    println("Population: $longValue")
+
+    // Float type
+    var pi: Float = 3.14f
+    println("Pi: $pi")
+
+    // Double type
+    var doubleValue: Double = 4.48486
+    println("Euler's number: $doubleValue")
+}
