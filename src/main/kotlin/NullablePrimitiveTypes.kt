37b6ee0282af48576575870280f082cce52dfc15
Sonal Dhage <sonal.dhage@thermondo.de>
2023-02-06 19:25:05 +0100

    Created an example for nullable primitive types

---
+++ src/main/kotlin/NullablePrimitiveTypes.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var nullableInt: Int? = null
+    var nullableDouble: Double? = null
+    var nullableBoolean: Boolean? = null
+
+    nullableInt = 10
+    nullableDouble = 3.14
+    nullableBoolean = true
+
+    println("nullableInt: $nullableInt")
+    println("nullableDouble: $nullableDouble")
+    println("nullableBoolean: $nullableBoolean")
+}
