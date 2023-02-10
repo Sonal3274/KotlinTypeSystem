08ca743aeaa9d299b29abe03bb936e73a5e33e28
Sonal Dhage <sonal.dhage@thermondo.de>
2023-02-10 07:07:25 +0100

    Created an example for Arrays of Objects and primitive types

---
+++ src/main/kotlin/ArrayOfObjectsAndPrimitiveTypes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val names = arrayOf("Alex", "Jason", "Fiona")
+    println(names.contentToString())
+
+    val numbers = intArrayOf(7, 8, 9, 34, 43)
+    println(numbers.contentToString())
+}
+
+
